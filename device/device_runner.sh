b4f1c3a90c6557c9d063190f1407bbc92d8499ac
Massimiliano Bartoli <mbartoli@upmem.com>
2022-04-05 17:54:50 +0200

    SEC-14: add scp for remote communication
    Signed-off-by: Massimiliano Bartoli <mbartoli@upmem.com>

--- device/device_runner.sh
+++ device/device_runner.sh
@@ -1,4 +1,5 @@
 #/bin/sh -x
+#expected to be in Nice
 echo "Environment cleaning"
 modprobe pim
 rm dpu_app_device.tar
@@ -16,4 +17,6 @@ do
     sleep 1
 done
 echo "Sending encrypted sensor data to the server"
-cp temp_sample ../server
+#cp temp_sample ../server
+#share to Rennes IP address
+scp -P 1535 temp_sample root@2.10.21.94:/home/root/tandem_demo/server/
--- server/server_runner.sh
+++ server/server_runner.sh
@@ -1,4 +1,5 @@
 #/bin/sh -x
+#expected to be in Rennes
 echo "Environment cleaning"
 modprobe pim
 rm OK
@@ -10,4 +11,6 @@ do
     sleep 5
 done
 echo "Sending encrypted application to the device"
-cp dpu_app_device.tar ../device
+#cp dpu_app_device.tar ../device
+#share to Nice IP address
+scp -P 1535 dpu_app_device.tar root@77.197.106.105:/home/root/tandem_demo/device/
