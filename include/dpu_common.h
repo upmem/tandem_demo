0948ed12e17c7f416a30cf45add1198b6588d056
Massimiliano Bartoli <mbartoli@upmem.com>
2022-04-07 12:24:33 +0200

    SEC-14: user application handled in secure MRAM
    Signed-off-by: Massimiliano Bartoli <mbartoli@upmem.com>

--- include/dpu_common.h
+++ include/dpu_common.h
@@ -9,7 +9,6 @@
 #define P256_PUB_KEY_SIZE ((256/8)*2)
 #define P256_SIG_SIZE ((256/8)*2)
 #define SHA256_SIZE (256/8)
-#define SIG_DATA_SIZE (P256_PUB_KEY_SIZE + SHA256_SIZE + P256_SIG_SIZE)
 #define AES_BLOCK_SIZE (16)
 
 #define DPU_POLICY_VERIFY_AND_JUMP  (0)
@@ -18,6 +17,7 @@
 #define VERIFICATION_STATUS_FAILURE (-1)
 #define VERIFICATION_STATUS_SUCCESS (0)
 
+#define ATF_LOG_BUFFER_SIZE         (80)
 typedef struct {
     unsigned int dpu_policy __dma_aligned;
     volatile unsigned long int verification_status __dma_aligned;
@@ -32,6 +32,13 @@ typedef struct {
     volatile uint8_t encrypted_device_temp_sample[AES_BLOCK_SIZE];
     uint8_t		 code[] __dma_aligned;
 } mram_t;
+
+typedef struct {
+    /* log buffer handled by ATF */
+    char atf_log_buffer [ATF_LOG_BUFFER_SIZE];
+    uint8_t hash[SHA256_SIZE] __dma_aligned ;
+    uint8_t app_text[APP_MAX_SIZE]  __dma_aligned;
+} sec_mram_t;
 
 void wait (uint32_t seconds);
 #endif /* DPU_COMMON_H */
--- pilot/dpu_hash_pilot.c
+++ pilot/dpu_hash_pilot.c
@@ -6,10 +6,16 @@
 #include "dpu_common.h"
 
 __mram_noinit mram_t mram;
+extern __mram_ptr void *__sys_sec_mram_start;
 
 int main (void){
+    __mram_ptr sec_mram_t *sec_mram = (__mram_ptr sec_mram_t *)__sys_sec_mram_start;
     __dma_aligned uint8_t out_hash[SHA256_SIZE];
     sha256((const u8 *)(uint32_t) mram.app_text, mram.app_text_size, (u8 *)out_hash);
+
+    /* Copy the hash to secure MRAM for further handling - accessible by DPUs only */
+    mram_write(out_hash, sec_mram->hash, SHA256_SIZE);
+    /* Copy the hash to non-secure MRAM for sanity checks - for demonstration purposes only */
     mram_write(out_hash, mram.hash, SHA256_SIZE);
     return 0;
 }
